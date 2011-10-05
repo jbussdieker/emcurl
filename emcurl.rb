2597217e3cc74be3b19f01c66f8b2c1ac08b8536
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2011-10-04 23:23:47 -0700

    first commit

---
+++ emcurl.rb
@@ -0,0 +1,29 @@
+require 'eventmachine'
+ARGV[0] = "www.betaqa.lifetechnologies.com"
+ARGV[1] = "80"
+
+class HTTP < EventMachine::Connection
+  def post_init
+    @data = ""
+    request = File.read("header")
+    send_data request
+  end
+  def receive_data(data)
+    @data << data
+  end
+  def unbind
+    puts "Recieved #{@data.length} bytes"
+    puts "Connection closed"
+    headers, payload = @data.split("\r\n\r\n", 2)
+    puts headers
+    File.open("output.html.gz", 'w') {|f| f.write(payload) }
+    `gzip -d -f output.html.gz`
+    EventMachine::stop_event_loop
+  end
+end
+
+puts "Connecting to #{ARGV[0]} port #{ARGV[1]}"
+EventMachine.run {
+  EventMachine.connect ARGV[0], ARGV[1], HTTP
+}
+
